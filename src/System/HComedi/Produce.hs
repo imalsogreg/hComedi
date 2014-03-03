9a7634c943c80e05a511119c7f515e6421eda22d
Greg Hale <imalsogreg@gmail.com>
2014-03-03 19:04:17 +0000

    sync from laptop

--- src/System/HComedi/Produce.hs
+++ src/System/HComedi/Produce.hs
@@ -6,11 +6,12 @@ import System.HComedi.Units
 import System.HComedi.Command
 import qualified System.HComedi.ComediBase as B
 
-import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Storable as S
+import qualified Data.Vector as V
 import Pipes
 import qualified Pipes.Prelude as P
 
-unSputter :: Monad m => Int -> Pipe (U.Vector LSample) (U.Vector LSample) m r
+unSputter :: Monad m => Int -> Pipe (S.Vector LSample) (S.Vector LSample) m r
 unSputter nTarget = aux $ fromList []
   where
     aux acc
@@ -22,10 +23,27 @@ unSputter nTarget = aux $ fromList []
         samples' <- await
         aux (acc U.++ samples')
 
-toChannels :: Monad m => Int -> Int ->
-              Pipe  (U.Vector LSample) (V.Vector (U.Vector LSample)) m r
-toChannels nChans nSampsPerChan =
-  
+subdivide :: Int -> S.Vector LSample -> V.Vector (S.Vector LSample)
+subdivide subVecLength v =
+  let subSlice i = V.slice v (i*subVecLength) ((i+1)*subVecLength-1)
+  in V.map subSlice [0 .. (length v `div` subVecLength)-1]
+
+subdivideTrans :: Int -> SVector LSample -> Vector (S.Vector LSample)
+subdivideTrans subVecLength v =
+  let nSubVec = V.length v `div` subVecLength
+    subinds0 = [0, (V.length v `div` subVecLength)-1 .. 
+    subSplice i = V.map 
+
+toChannels :: Monad m => Bool -> Int -> Int ->
+              Pipe  (S.Vector LSample) (V.Vector (S.Vector LSample)) m r
+toChannels transpose nChans nSampsPerChan =
+  let nSampsPerBuffer = nChans nSampsPerChan
+      aux = do
+        buf <- await
+        
+        let result = if   transpose
+                     then 
+  in aux
 
 produceAsChannel :: Handle -> ValidCommand -> Producer (U.Vector LSample)
 produceAsChannel handle cmd = do
